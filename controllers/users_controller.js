4d79ffe59f909dbbc3104ceaf4f8a2493150ea97
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2024-10-27 18:32:06 +0100

    Update users_controller.js captcha-verify original function

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -276,14 +276,15 @@ exports.loginUser = async (req, res) => {
     
 	try {
         // Send the token to the CAPTCHA provider (hCaptcha, reCAPTCHA) for verification
-        /*
+        
 	const response = await axios.post('https://hcaptcha.com/siteverify', null, {
             params: {
                 secret: CAPTCHA_SECRET,   // Your secret key for CAPTCHA verification
                 response: captchaToken    // The token received from the client
             }
         });
-	*/
+	
+	/*
 	const response = await axios.post(
             'https://hcaptcha.com/siteverify',
             new URLSearchParams({
@@ -296,6 +297,7 @@ exports.loginUser = async (req, res) => {
                 },
             }
         );
+	*/
 		
 	console.log('verify captcha : ', response.data); // Check for errors or unexpected responses
 
