8f7f12b5d51f8b366439dfc9968f91f2eb022653
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2024-11-12 17:21:07 +0100

    Update fcm.js

--- routes/fcm.js
+++ routes/fcm.js
@@ -4,7 +4,9 @@ const router  			= express.Router();
 const fcmController	= require('../controllers/fcm_tokens_controller'); // Point to your controller
 
 // Define routes
-router.post('/all-fcm-tokens', fcmController.getAllFCMTokens);   // POST /fcm/all-fcm-tokens
+router.post('/post-all-fcm-tokens', fcmController.postAllFCMTokens);   // POST /fcm/all-fcm-tokens
+router.get('/get-all-fcm-tokens',   fcmController.getAllFCMTokens);    // GET /fcm/all-fcm-tokens
+
 //router.get('/:id', usersController.getUser);                   // GET /users/:id
 //router.put('/:id', usersController.updateUser);                // PUT /users/:id
 
