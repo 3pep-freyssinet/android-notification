03f1d410eb32bb786fa447ebc8fcb8a68f27d406
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2024-11-30 07:02:12 +0100

    Update notifications.js

--- routes/notifications.js
+++ routes/notifications.js
@@ -1,1 +1,13 @@
 
+// routes/fcm.js
+const express 			          = require('express');
+const router  			          = express.Router();
+const notificationController	= require('../controllers/notifications_controller'); 
+
+console.log('notifications routes');
+
+// Define routes
+router.post('/send-to-all', notificationController.send-to-all);   // POST /notifications/send-to-all
+
+// Export the router
+module.exports = router;
