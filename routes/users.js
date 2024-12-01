f60451b6155095f4231e9a9d26b45f44fcaf96ab
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2024-12-01 07:38:57 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,14 @@
 // routes/users.js
-const express = require('express');
-const router  = express.Router();
+const express         = require('express');
+const router          = express.Router();
 const usersController = require('../controllers/users_controller'); // Point to your controller
+
+console.log(' routes users ');
 
 // Define routes
 router.post('/register', usersController.registerUser);   // POST /users/register
 router.post('/login',    usersController.loginUser);      // POST /users/login
+
 router.get('/:id', 		 usersController.getUser);              // GET /users/:id
 router.put('/:id', 		 usersController.updateUser);           // PUT /users/:id
 
