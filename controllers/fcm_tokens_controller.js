8ec31a35515e9c07b255c453051d96ab8a84ac0f
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2024-12-02 07:39:22 +0100

    Update fcm_tokens_controller.js

--- controllers/fcm_tokens_controller.js
+++ controllers/fcm_tokens_controller.js
@@ -60,7 +60,7 @@ console.log('storeFCMTokens : user_id = ', userId, ' fcm_token = ', fcm_token, '
 	
     console.log('storeFCMTokens / : result : ', JSON.stringify(result));
 
-    if(result.rowsCount == 1){
+    if(result.rowCount == 1){
 	console.log('storeFCMTokens successfull');
     	res.status(200).json({ 
 			message: 'fcm registered successfully', 
