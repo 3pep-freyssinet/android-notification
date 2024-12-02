6ccc7a9258c93053a268515837d7ceb5aeabf9b4
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2024-12-02 15:25:05 +0100

    Update notifications_controller.js

--- controllers/notifications_controller.js
+++ controllers/notifications_controller.js
@@ -10,6 +10,9 @@ const admin  = require('firebase-admin');
 // Initialize Firebase Admin SDK
 //const serviceAccount = require('../android-firebase-634a8-firebase-adminsdk-ggw45-6b2ec92cde.json');
 const serviceAccount = require('../android-firebase-634a8-firebase-adminsdk-ggw45-2d2529b087.json');
+
+console.log('notifications_controller : serviceAccount : ', serviceAccount);
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
