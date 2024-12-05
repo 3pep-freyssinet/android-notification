537c3fc98b359e8d7834520b99eeb6e3d53ba665
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2024-12-05 16:53:12 +0100

    Update pins_controller.js

--- controllers/pins_controller.js
+++ controllers/pins_controller.js
@@ -24,42 +24,8 @@ exports.getPins = async (req, res) => {
 	const pins = req.body.pins;
 	
 	console.log("get pins : pins : ", pins );
-	
-    // Verify the refresh token
-	const {userId, expires_at} = await verifyRefreshToken(refreshToken);
-	
-	console.log("refresh-jwt-token : refreshToken : userId = ",userId, " expires_at = ", expires_at);
-	
-	// Create a Date object from the string
-    const date = new Date(expires_at);
 
-    // Subtract 3 days (3 days * 24 hours * 60 minutes * 60 seconds * 1000 milliseconds)
-    date.setTime(date.getTime() - ALERT_TIME);
+	    
+	
 
-    // Print the new date
-    const triggerTime = date.toISOString();
-	
-	var isRefreshTokenExpired = Date.now() > triggerTime;
-	
-	console.log("refresh-jwt-token : is refreshToken expired  : ", isRefreshTokenExpired );
-	
-	var newRefreshToken;
-	if(isRefreshTokenExpired){//the 'refresh-token' is not expired but it remains less than 3 days to dead date.
-		//Generate new 'refresh-token'
-		newRefreshToken = generateRefreshToken();
-		
-		//update the db
-		await updateRefreshToken(userId, newRefreshToken);
-	}
-	
-	console.log("refresh-jwt-token : newRefreshToken : ",newRefreshToken);
-	
-	// Generate a new jwt token (JWT)
-    newJWTToken = jwt.sign({ userId: userId }, JWT_SECRET, { expiresIn: JWT_EXPIRY });
-    res.json({ new_jwt_token: newJWTToken, new_refresh_token: newRefreshToken, new_refresh_token_expiry: REFRESH_EXPIRY});
-			
-	//update the jwt_tokens table
-	if(newJWTToken != null){
-		const result = await updateJWTToken(userId, newJWTToken);
-	}
 };
