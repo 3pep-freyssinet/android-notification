dc9129676704133c90ff2e7e74fc9acced7a38c5
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2024-12-08 07:36:14 +0100

    Update pins_controller.js

--- controllers/pins_controller.js
+++ controllers/pins_controller.js
@@ -57,7 +57,7 @@ try {
 // Fetch Certificate (Logic Only)
 exports.fetchCertificate = async (req, res) => {
     console.log('fetchCertificate : start');
-	return;
+
     try {
         const domain = req.query.domain || 'android-notification.onrender.com'; // Accept domain as query param
 
@@ -143,7 +143,6 @@ exports.storeCertificate = async (domain, sha256Fingerprint) => {
 // Fetch and Store Certificate
 exports.fetchStoreCertificate = async (req, res) => {
 	console.log('fetchStoreCertificate : start');
-	return;
 	
     try {
         // Step 1: Fetch Certificate
