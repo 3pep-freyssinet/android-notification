ed9f469258383372b5d1f4106366f15c3fcc46f4
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2024-12-08 13:30:09 +0100

    Update pins_controller.js

--- controllers/pins_controller.js
+++ controllers/pins_controller.js
@@ -1,3 +1,8 @@
+/* test
+curl -X GET -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiY3Jvbi1qb2Itc2VydmljZSIsInJvbGUiOiJjcm9uIiwiaWF0IjoxNzMzNjQ4NjkxLCJleHAiOjE3MzYyNDA2OTF9.Y-vu3nzNmwoJQT-hKeJstQ17LpEqdkuy-xlWzhbOYIE" 
+-H "Content-Type: application/json" 
+https://android-notification.onrender.com/pins/fetch-store-certificate
+*/
 
 require('dotenv').config();
 const pool   = require('../db'); // Assuming you use a database pool for Postgres or MySQL
