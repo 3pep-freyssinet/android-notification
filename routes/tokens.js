2853e0c14cca09a5da649ca01c73a5dadde69697
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2024-12-09 07:20:06 +0100

    Update tokens.js

--- routes/tokens.js
+++ routes/tokens.js
@@ -1,11 +1,12 @@
 // routes/tokens.js
-const express 			= require('express');
-const router  			= express.Router();
+const express 			    = require('express');
+const router  			    = express.Router();
 const tokensController	= require('../controllers/tokens_controller'); // Point to your controller
+const authMiddleware    = require('../middleware/auth');
 
 // Define routes
-router.post('/refresh-jwt-token', tokensController.refreshJWTToken);   // POST /tokens/refresh-jwt-token
-router.post('/renew-tokens', tokensController.renewTokens);            // POST /tokens/renew-tokens
+router.post('/refresh-jwt-token', authMiddleware, tokensController.refreshJWTToken);   // POST /tokens/refresh-jwt-token
+router.post('/renew-tokens', authMiddleware, tokensController.renewTokens);            // POST /tokens/renew-tokens
 
 //router.get('/:id', usersController.getUser);              // GET /users/:id
 //router.put('/:id', usersController.updateUser);           // PUT /users/:id
