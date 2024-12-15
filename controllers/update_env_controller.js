1b01fbe95443fab35b23c7152d5b856c95c8690f
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2024-12-15 07:09:44 +0100

    Update update_env_controller.js

--- controllers/update_env_controller.js
+++ controllers/update_env_controller.js
@@ -1,7 +1,18 @@
+import renderApi from '@api/render-api';
 
-exports.storeFCMToken = async (req, res) => {
+exports.updateEnv = async (req, res) => {
 
-   console.log('fcm tokens : store fcm token');
+   console.log('update Env start ');
 	
-   // Extract token and user information
-   const { fcm_token } = req.body;
+renderApi.auth('rnd_0zPNWnTmGysVCH6oECy29bMhX6iy');
+renderApi.updateEnvVar({value: 'aaaaa'}, {
+  serviceId: 'srv-cseq2m5svqrc73f7ai5g',
+  envVarKey: 'JWT_SECRET'
+})
+  .then(({ data }) => {
+	  console.log(data));
+
+  }
+  .catch(err => console.error(err)
+	
+	);
