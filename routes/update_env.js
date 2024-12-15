6892e0e0eacd058565bcd2465da6609766590093
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2024-12-15 07:02:22 +0100

    Update update_env.js

--- routes/update_env.js
+++ routes/update_env.js
@@ -1,1 +1,10 @@
+// routes/update_env.js
+const express 			= require('express');
+const router  			= express.Router();
+const updateEnvController	= require('../controllers/update_env_controller'); // Point to your controller
+const authMiddleware = require('../middleware/auth');
 
+console.log('routes : update_env');
+
+// Define routes
+router.put('/update-env', updateEnvController.updateEnv);   // PUT /update_env/update-env
