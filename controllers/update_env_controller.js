7304241d06b09695cb1b6d0e1c7721c26e6d2b39
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2024-12-15 07:15:06 +0100

    Update update_env_controller.js

--- controllers/update_env_controller.js
+++ controllers/update_env_controller.js
@@ -2,17 +2,21 @@ import renderApi from '@api/render-api';
 
 exports.updateEnv = async (req, res) => {
 
-   console.log('update Env start ');
+   	console.log('update Env start ');
 	
-renderApi.auth('rnd_0zPNWnTmGysVCH6oECy29bMhX6iy');
-renderApi.updateEnvVar({value: 'aaaaa'}, {
-  serviceId: 'srv-cseq2m5svqrc73f7ai5g',
-  envVarKey: 'JWT_SECRET'
-})
-  .then(({ data }) => {
-	  console.log(data));
-
-  }
-  .catch(err => console.error(err)
-	
+	renderApi.auth('rnd_0zPNWnTmGysVCH6oECy29bMhX6iy');
+	renderApi.updateEnvVar({value: 'ccccc'}, {
+	  serviceId: 'srv-cseq2m5svqrc73f7ai5g',
+	  envVarKey: 'JWT_SECRET'
+	})
+	  .then(({ data }) => {
+		  console.log(data));
+		res.status(200).json({ 
+				message: 'env variable modified successfully', 
+				data:data
+			        });
+	  }
+  	.catch(err => console.error(err);
+		res.status(500).send('Internal server error : Error env variable modification');
 	);
+}
