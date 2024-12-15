a9e166bdcee00fd040f6fb3eb035f40ab03eee87
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2024-12-15 17:58:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -124,8 +124,8 @@ const users_routes  		= require('./routes/users');
 const tokens_routes 		= require('./routes/tokens');
 const fcm_routes    		= require('./routes/fcm');
 const notifications_routes    	= require('./routes/notifications');
-const pins_routes    	        = require('./routes/pins');
-const env_routes    	        = require('./routes/environment');
+//const pins_routes    	        = require('./routes/pins');
+const env_routes    	        = require('./routes/environ');
 
 // Use routes
 app.use('/users', users_routes);
@@ -133,7 +133,7 @@ app.use('/tokens', tokens_routes);
 app.use('/fcm', fcm_routes);
 app.use('/notifications', notifications_routes);
 //app.use('/pins', pins_routes);
-app.use('/environment', env_routes);
+app.use('/environ', env_routes);
 
 // Set EJS as the template engine
 app.set('view engine', 'ejs');
