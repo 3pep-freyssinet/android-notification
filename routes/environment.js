51a2e1e5ef0b45caf24181b63a1270b68afbc5e7
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2024-12-15 17:13:57 +0100

    Update and rename env.js to environment.js

--- routes/environment.js
+++ routes/environment.js
@@ -1,10 +1,10 @@
-// routes/env.js
+// routes/environment.js
 const express 			= require('express');
 const router  			= express.Router();
 const envController	= require('../controllers/env_controller'); // Point to your controller
 const authMiddleware = require('../middleware/auth');
 
-console.log('routes : env');
+console.log('routes : environment');
 
 // Define routes
 router.put('/update-env', envController.updateEnv);   // PUT /update_env/update-env
