b0fedda3e152efc63dd05e6dda68e860a0aaf87d
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2024-12-19 18:36:27 +0100

    Update tokens.js

--- routes/tokens.js
+++ routes/tokens.js
@@ -5,9 +5,9 @@ const tokensController	= require('../controllers/tokens_controller'); // Point t
 const authMiddleware    = require('../middleware/auth');
 
 // Define routes
-router.post('/refresh-jwt-token', authMiddleware, tokensController.refreshJWTToken);                 // POST /tokens/refresh-jwt-token
-router.post('/renew-tokens', authMiddleware, tokensController.renewTokens);                          // POST /tokens/renew-tokens
-router.post('/update-jwt-env', authMiddleware, tokensController.updateJWTEnvironment);               // POST /tokens/update-jwt_env
+router.post('/refresh-jwt-token', authMiddleware, tokensController.refreshJWTToken);                   // POST /tokens/refresh-jwt-token
+router.post('/renew-tokens', authMiddleware, tokensController.renewTokensHandler);                     // POST /tokens/renew-tokens
+router.post('/update-jwt-env', authMiddleware, tokensController.updateJWTEnvironment);                 // POST /tokens/update-jwt_env
 router.post('/renew-jwt-update-env', authMiddleware, tokensController.renewTokensUpdateJWTEnvironment);// POST /tokens/renew-jwt-update-env
 
 //router.get('/:id', usersController.getUser);              // GET /users/:id
