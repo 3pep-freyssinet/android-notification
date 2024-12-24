59a32ffa1a6ee75947fda627941d3926b7d2adad
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2024-12-24 08:09:29 +0100

    Update users_controller.js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -137,7 +137,7 @@ exports.registerUser = async (req, res) => {
 
 	    
 	//save refresh Token in database
-	const save_refresh_token = await storeRefreshTokenInDatabase(user, refresh_token, refresh_expires_at);
+	//const save_refresh_token = await storeRefreshTokenInDatabase(user, refresh_token, refresh_expires_at);
 	    
        console.log('registered : user : ', user, ' refresh_token : ', refresh_token, ' expires_at : ', expires_at);
 	    
