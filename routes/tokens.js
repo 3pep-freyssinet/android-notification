a357875528caf9bc168490dfb3fafd562b28e32a
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2024-12-25 17:43:42 +0100

    Update tokens.js

--- routes/tokens.js
+++ routes/tokens.js
@@ -1,13 +1,14 @@
 // routes/tokens.js
-const express 			    = require('express');
-const router  			    = express.Router();
+const express 	           = require('express');
+const router  		= express.Router();
 const tokensController	= require('../controllers/tokens_controller'); // Point to your controller
-const authMiddleware    = require('../middleware/auth');
+const authMiddleware             = require('../middleware/auth');
+const auth_refresh_middleware    = require('../middleware/auth_refresh');
 
 console.log("routes/tokens");
 
 // Define routes
-router.post('/refresh-jwt-token', authMiddleware, tokensController.refreshJWTToken);                   // POST /tokens/refresh-jwt-token
+router.post('/refresh-jwt-token', auth_refresh_middleware, tokensController.refreshJWTToken);          // POST /tokens/refresh-jwt-token
 router.post('/renew-tokens', authMiddleware, tokensController.renewTokensHandler);                     // POST /tokens/renew-tokens
 router.post('/renew-all-tokens', tokensController.renewAllTokensHandler);                              // POST /tokens/renew-all-tokens
 router.post('/update-jwt-env', authMiddleware, tokensController.updateJWTEnvironment);                 // POST /tokens/update-jwt_env
