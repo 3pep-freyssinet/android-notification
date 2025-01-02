69f2ba4018bb5d27a4bb2119d8e3334aa6c35aeb
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2025-01-02 08:10:34 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,11 @@ const usersController = require('../controllers/users_controller'); // Point to 
 console.log(' routes users ');
 
 // Define routes
-router.post('/register', usersController.registerUser);   // POST /users/register
-router.post('/login',    usersController.loginUser);      // POST /users/login
+router.post('/register', usersController.registerUser);             // POST /users/register
+router.post('/login',    usersController.loginUser);                // POST /users/login
 
-router.get('/:id', 		 usersController.getUser);              // GET /users/:id
-router.put('/:id', 		 usersController.updateUser);           // PUT /users/:id
+router.get('/:id', 		 usersController.getUser);                    // GET /users/:id
+//router.put('/:id', 		 usersController.updateUser);                 // PUT /users/:id
 
 router.post('/verify-captcha', usersController.verifyCaptcha);      // POST /users/verify-captcha
 
