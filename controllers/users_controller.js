d1c09772a9945521ac8aa344951d604479835db9
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2025-01-07 19:40:32 +0100

    Update users_controller.js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -455,6 +455,9 @@ exports.getStoredSharedPreferences = async (req, res) => {
 
 exports.setLockoutStatus = async (req, res) => {
     const username = 'Name147';
+    console.log('Headers:', req.headers); // Inspect headers
+    console.log('Body:', req.body);       // Inspect body
+	
     const {androidId, failedAttempts, lockoutUntil } = req.body;
 
     try {
