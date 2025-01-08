e506c5a72b723bc08ad8faf6e9df0021ddb42800
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2025-01-08 07:57:17 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -9,9 +9,9 @@ console.log(' routes users ');
 router.post('/register', usersController.registerUser);             // POST /users/register
 router.post('/login',    usersController.loginUser);                // POST /users/login
 
-router.get('/user_id',  usersController.getUserId);                // GET /users/user_id?user_id=
+router.get('/user_id',  usersController.getUserId);                 // GET /users/user_id?user_id=
 router.get('/get-stored-shared-preferences',  usersController.getStoredSharedPreferences);    // GET /users/get-stored-shared-preferences?android-id=
-router.post('/set-lockout-status',  usersController.setLockoutStatus);      // POST /users/set-lockout-status
+router.post('/set-lockout-status',    usersController.setLockoutStatus);    // POST /users/set-lockout-status
 router.post('/reset-lockout-status',  usersController.resetLockoutStatus);  // POST /users/reset-lockout-status
 
 //router.get('/:id', 		   usersController.getUser);                    // GET /users/:id
