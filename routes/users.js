cc36824e13a850aa9ebbdb0f2ae0a8b97bd1d883
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2025-01-12 16:02:15 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@
 const express         = require('express');
 const router          = express.Router();
 const usersController = require('../controllers/users_controller'); // Point to your controller
+const authMiddleware  = require('../middleware/auth');
 
 console.log(' routes users ');
 
@@ -9,10 +10,10 @@ console.log(' routes users ');
 router.post('/register', usersController.registerUser);             // POST /users/register
 router.post('/login',    usersController.loginUser);                // POST /users/login
 
-router.get('/user_id',                        usersController.getUserId);                     // GET /users/user_id?user_id=
-router.get('/get-stored-shared-preferences',  usersController.getStoredSharedPreferences);    // GET /users/get-stored-shared-preferences?android-id=
-router.post('/set-lockout-status',            usersController.setLockoutStatus);              // POST /users/set-lockout-status
-router.post('/reset-lockout-status',          usersController.resetLockoutStatus);            // POST /users/reset-lockout-status
+router.get('/user_id',                                usersController.getUserId);                     // GET /users/user_id?user_id=
+router.get('/get-stored-shared-preferences',          usersController.getStoredSharedPreferences);    // GET /users/get-stored-shared-preferences?android-id=
+router.post('/set-lockout-status', authMiddleware,    usersController.setLockoutStatus);              // POST /users/set-lockout-status
+router.post('/reset-lockout-status',  authMiddleware, usersController.resetLockoutStatus);            // POST /users/reset-lockout-status
 
 //router.get('/:id', 		   usersController.getUser);                 // GET /users/:id
 //router.put('/:id', 		   usersController.updateUser);              // PUT /users/:id
