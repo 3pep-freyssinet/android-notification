abbc3d36dcddf5ed2d502166f3cd9c15bb1f3f53
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2025-01-12 15:56:39 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -9,15 +9,15 @@ console.log(' routes users ');
 router.post('/register', usersController.registerUser);             // POST /users/register
 router.post('/login',    usersController.loginUser);                // POST /users/login
 
-router.get('/user_id',  usersController.getUserId);                 // GET /users/user_id?user_id=
+router.get('/user_id',                        usersController.getUserId);                     // GET /users/user_id?user_id=
 router.get('/get-stored-shared-preferences',  usersController.getStoredSharedPreferences);    // GET /users/get-stored-shared-preferences?android-id=
-router.post('/set-lockout-status',    usersController.setLockoutStatus);    // POST /users/set-lockout-status
-router.post('/reset-lockout-status',  usersController.resetLockoutStatus);  // POST /users/reset-lockout-status
+router.post('/set-lockout-status',            usersController.setLockoutStatus);              // POST /users/set-lockout-status
+router.post('/reset-lockout-status',          usersController.resetLockoutStatus);            // POST /users/reset-lockout-status
 
-//router.get('/:id', 		   usersController.getUser);                    // GET /users/:id
-//router.put('/:id', 		 usersController.updateUser);                   // PUT /users/:id
+//router.get('/:id', 		   usersController.getUser);                 // GET /users/:id
+//router.put('/:id', 		   usersController.updateUser);              // PUT /users/:id
 
-router.post('/verify-captcha', usersController.verifyCaptcha);          // POST /users/verify-captcha
+router.post('/verify-captcha', usersController.verifyCaptcha);       // POST /users/verify-captcha
 
 // Export the router
 module.exports = router;
