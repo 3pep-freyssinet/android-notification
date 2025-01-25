036c46d7452c3a6e9a17fcda77bb2ee44fd8a1b7
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2025-01-25 10:01:35 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -7,9 +7,9 @@ const authMiddleware  = require('../middleware/auth');
 console.log(' routes users ');
 
 // Define routes
-router.post('/register', usersController.registerUser);                     // POST /users/register
-router.post('/login',    usersController.loginUser);                        // POST /users/loginchangePassword
-router.post('/change-password', authMiddleware, usersController.loginUser); // POST /users/change-password
+router.post('/register', usersController.registerUser);                           // POST /users/register
+router.post('/login',    usersController.loginUser);                              // POST /users/loginchangePassword
+router.post('/change-password', authMiddleware, usersController.changePassword);  // POST /users/change-password
 
 router.get('/user_id',                                usersController.getUserId);                     // GET /users/user_id?user_id=
 router.get('/get-stored-shared-preferences',          usersController.getStoredSharedPreferences);    // GET /users/get-stored-shared-preferences?android-id=
