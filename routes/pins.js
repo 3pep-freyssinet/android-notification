8345bd385951a2dff2fcad88c1ac886ccc3e11bb
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2025-01-30 18:41:41 +0100

    Update pins.js

--- routes/pins.js
+++ routes/pins.js
@@ -15,15 +15,12 @@ router.post('/renew-certificate', authMiddleware, pinsController.renewSHA256Cert
 router.get('/get-pins', authMiddleware, pinsController.getPins);                           // GET /pins/get-pins
 router.get('/get-latest-sha256pin', authMiddleware, pinsController.getLatestSHA256Pin);    // GET /pins/get-latest-sha256pin
 
-/*
-//used in "cron-job"
-router.get('/fetch-certificate', authMiddleware, pinsController.fetchCertificate);                // GET /pins/fetch-certificate
-router.post('/store-certificate', authMiddleware, pinsController.storeCertificate);               // POST /pins/store-certificate
-router.post('/fetch-store-certificate', authMiddleware, pinsController.fetchStoreCertificate);     // POST /pins/fetch-store-certificate
-*/
+router.get('/fetch-certificate',        authMiddleware, pinsController.fetchCertificate);       // GET /pins/fetch-certificate
+router.post('/store-certificate',       authMiddleware, pinsController.storeCertificate);       // POST /pins/store-certificate
+router.post('/fetch-store-certificate', authMiddleware, pinsController.fetchStoreCertificate);  // POST /pins/fetch-store-certificate
 
-//router.get('/get-all-fcm-tokens',   fcmController.getAllFCMTokens);    // GET /fcm/get-all-fcm-tokens
-//router.post('/store-fcm-tokens',    fcmController.storeFCMTokens);     // POST /fcm/store-fcm-tokens
+//router.get('/get-all-fcm-tokens',   fcmController.getAllFCMTokens);                // GET /fcm/get-all-fcm-tokens
+//router.post('/store-fcm-tokens',    fcmController.storeFCMTokens);                 // POST /fcm/store-fcm-tokens
 //router.post('/store-fcm-token',     authMiddleware, fcmController.storeFCMToken);  // POST /fcm/store-fcm-tokens
 
 
