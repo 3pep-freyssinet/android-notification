1e6fc6d490468b81fde6f81710b60bcf83b1e98c
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2025-03-02 03:58:27 +0100

    Update users_controller.js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -84,7 +84,8 @@ exports.forgotPassword = async (req, res) => {
     `, [userId, resetToken, tokenExpiry]);
 
     // Send reset email using nodemailer (configure your transporter)
-    
+
+    /*
     //Google mail
     const transporter = nodemailer.createTransport({
       // e.g., SMTP configuration or a service like SendGrid
@@ -92,9 +93,9 @@ exports.forgotPassword = async (req, res) => {
       //auth: { user: 'your-email@gmail.com', pass: 'your-password' }
       auth: { user: 'beldi.chergui@gmail.com', pass: 'FFaaDDaa@24110' }
     });
-    
+    */
    
-/*	  
+  
   //Yahoo mail
   const transporter = nodemailer.createTransport({
   host: 'smtp.mail.yahoo.com',
@@ -105,8 +106,8 @@ exports.forgotPassword = async (req, res) => {
     pass: 'faddafadda',            //process.env.YAHOO_PASS  // your Yahoo app password (if using 2FA)
   }
 });
-*/
-    if(true)res.json({ message: 'Password reset email sent' });
+
+    //if(true)res.json({ message: 'Password reset email sent' });
 	  
     const resetLink = `https://android-notification.onrender.com/users/forgot-password?token=${resetToken}&user=${userId}`;
     const email_ = 'tomcat.suser@yahoo.fr';
