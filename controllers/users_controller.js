8f106f15e46859817280b22771a545a5785b2f87
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2025-04-15 13:42:23 +0100

    Update users_controller.js

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -220,8 +220,16 @@ exports.resetPassword = async (req, res) => {
     //if(true)return;
 	  
     // Hash the new password (using bcrypt)
-    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    const hashedNewPassword = await bcrypt.hash(newPassword, 10);
     
+    //get the id from the req
+    const userId = req.user.userId;
+	
+    console.log('resetPassword : userId : ', userId);
+    
+    //check if it is already used
+    const isnewPasswordValid = await isNewPasswordValid(userId, newPassword);
+	  
     // Update the user's password in the users table
     await pool.query(`UPDATE users_notification SET password = $1 WHERE id = $2`, [hashedPassword, userId]);
     
@@ -245,6 +253,58 @@ exports.resetPassword = async (req, res) => {
         });	  
   }
 };
+
+async function isNewPasswordValid(userId, newPassword){
+try{
+    // Fetch stored password hash and last changed date
+    const userQuery = `
+        SELECT password, last_password_changed 
+        FROM users_notification 
+        WHERE id = $1
+    `;
+    const userResult     = await pool.query(userQuery, [userId]);
+    const storedPassword = userResult.rows[0]?.password; //the current password
+
+    /*
+    //check the validity of the provided current password 'current password' against the stored password 'stored password'.
+    // Compare the provided clear current password with the hashed password stored in the database.
+
+	const isPasswordValid = await bcrypt.compare(password, storedPassword);
+         
+	console.log('matchPassword : isPasswordValid : ', isPasswordValid);
+		
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: 'Invalid username or password' });
+        }
+	*/
+	   
+    // Get all password stored in 'password_history'. 
+    const historyQuery = `
+        SELECT password 
+        FROM password_history 
+        WHERE user_id = $1
+    `;
+    const historyResult    = await pool.query(historyQuery, [userId]);
+    const previousPassword = historyResult.rows.map(row => row.password);
+
+    for (const hash of [storedPassword, ...previousPassword]) {
+	console.log('isNewPasswordValid : loop : hash : ', hash, ' password : ', newPassword); 
+	 const test =  await bcrypt.compare(newPassword, hash);
+	 console.log('isNewPasswordValid : loop : test : ', test);    
+        if (await bcrypt.compare(newPassword, hash)) {
+            //throw new Error('New password cannot be the same as the current or previous passwords.');
+	    console.error('isNewPasswordValid : New password cannot be the same as the current or previous passwords.');
+	    //return res.status(402).json({ message: 'New password cannot be the same as the current or previous passwords.' });
+            return false;
+	}
+    }
+     console.log('isNewPasswordValid : Password is valid.');
+     return true;
+  }catch(error){
+	console.error('isNewPasswordValid : Password is not valid.' + error);
+	return false;
+  }
+}
 
 
 // POST /users/forgot-password
@@ -817,9 +877,9 @@ exports.matchPassword = async (req, res) => {
     const historyResult    = await pool.query(historyQuery, [userId]);
     const previousPassword = historyResult.rows.map(row => row.password);
 
-    for (const hash of [storedPassword, ...previousPassword]) {
+    for (const hash of [storedPassword, ...previousPassword]) { //'storedPassword' is the cuurent password
 	console.log('changePassword : loop : hash : ', hash, ' password : ', password); 
-	 const test =  await bcrypt.compare(password, hash);
+	 const test =  await bcrypt.compare(password, hash);//compare clear with hash
 	 console.log('changePassword : loop : test : ', test);    
         if (await bcrypt.compare(password, hash)) {
             //throw new Error('New password cannot be the same as the current or previous passwords.');
