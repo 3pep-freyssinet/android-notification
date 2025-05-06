29e41111e519098965a53835727e3f6c47789eb6
3pep-freyssinet <45789072+3pep-freyssinet@users.noreply.github.com>
2025-05-06 13:48:09 +0100

    Update pins_controller.js

--- controllers/pins_controller.js
+++ controllers/pins_controller.js
@@ -626,7 +626,8 @@ try {
 	let userId;
 	if(!req.user){
 	    const androidId = req.body.androidId;
-	    console.log('fetchStoreCertificate : req.body : ', req.body);
+	    console.log('fetchStoreCertificate : req.body : ', JSON.stringify(req.body));
+		
 	    if(androidId == null){
 	       console.warn('fetchStoreCertificate : androidId not found.');
                return res.status(404).json({ message: 'androidId not found' });
